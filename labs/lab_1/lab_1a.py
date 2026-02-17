931a20e2bb11ce10c62940cd0d298f09e7054f27
fruitcustard <mimismile321@gmail.com>
2026-02-17 16:54:45 -0500

    Update lab_1a.py
    added intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Michelle" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Michelle Liu, and I'm a junior from Massachusetts. In my free time, I like to dance, hang out with my friends, and read -- my most recent favorite has been Cloud Cuckoo Land by Anthony Doerr.")
 
 if __name__ == "__main__":
     main()
